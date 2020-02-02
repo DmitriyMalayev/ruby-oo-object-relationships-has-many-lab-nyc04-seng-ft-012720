6e4982deeee3db06a768404c23f52f886166cdf5
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-02-02 23:47:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,17 @@
-class Song 
-    attr_accessor :artist, :name, :artist_name  
+class Song
+    attr_accessor :artist, :name, :artist_name
     @@all = []
 
     def initialize(name)
-        @name = name 
-        @@all << self 
-    end 
+        @name = name
+        @@all << self
+    end
 
-    def self.all 
-        @@all 
-    end 
+    def self.all
+        @@all
+    end
 
-    def self.artist_name 
+    def self.artist_name
 
 
 
