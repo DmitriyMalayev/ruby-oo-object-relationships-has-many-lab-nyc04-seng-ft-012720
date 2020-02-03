1fa19605dbb2011268d7842eef5433a5933df5b7
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-02-03 00:15:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,28 +22,3 @@ end
 
 
 end
-
-
-
-# # # # class Artist
-# # # #     attr_accessor :name
-
-# # # #     def initialize(name)
-# # # #         @name = name
-# # # #         @songs = []
-# # # #     end
-
-# # # # end
-
-# # # # class Artist
-# # # #     attr_accessor :name
-
-# # # #     def initialize(name)
-# # # #     @name = name
-# # # #     @songs = []
-# # # #     end
-
-# # # #     def add_song(song)
-# # # #     @songs << song
-# # # #     end
-# # # # end
